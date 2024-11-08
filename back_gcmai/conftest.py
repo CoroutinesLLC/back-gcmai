d61a4e417cd52d2ecf61d291529b33d9aeaa311f
ReddyTintaya <reddy.tintaya.dev@gmail.com>
2024-11-07 20:19:24 -0600

    Initial commit

---
+++ back_gcmai/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from back_gcmai.users.models import User
+from back_gcmai.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
